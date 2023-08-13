43cc341620c0444661feb186eeb8782e8261f28e
wxd_code <w15755583882022@163.com>
2023-08-13 13:38:56 +0800

    首页静态布局完结

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -92,20 +92,70 @@
 			</view>
 			<view class="mydoctor">
 				<view class="doctor_header">
-					我的医生
+					我的
+					<text class="light">医生</text>
 				</view>
 				<view class="doctor_bd">
 					<view class="doctorItem">
 						<view class="doctorImg">
-							
+							<image class="doctor_avator" src="../../static/logo.png" mode=""></image>
 						</view>
 						<view class="doctorInfo">
+							<view class="doctor_name">
+								<text class="names">林涛</text>
+								<text class="remark">5.0</text>
+							</view>
+							<view class="skill">
+								<text class="job_name">副主任医师</text>
+								<text class="productment">皮肤整形科</text>
+							</view>
 							
 						</view>
+						<view class="mark">
+							有号
+						</view>
 					</view>
+					
 					<view class="doctorItem">
-						外科专家
+						<view class="doctorImg">
+							<image class="doctor_avator" src="../../static/logo.png" mode=""></image>
+						</view>
+						<view class="doctorInfo">
+							<view class="doctor_name">
+								<text class="names">张碧晨</text>
+								<text class="remark">5.0</text>
+							</view>
+							<view class="skill">
+								<text class="job_name">副主任医师</text>
+								<text class="productment">外科</text>
+							</view>
+							
+						</view>
+						<view class="mark">
+							有号
+						</view>
 					</view>
+					
+					<view class="doctorItem">
+						<view class="doctorImg">
+							<image class="doctor_avator" src="../../static/logo.png" mode=""></image>
+						</view>
+						<view class="doctorInfo">
+							<view class="doctor_name">
+								<text class="names">刘亦菲</text>
+								<text class="remark">5.0</text>
+							</view>
+							<view class="skill">
+								<text class="job_name">副主任医师</text>
+								<text class="productment">心脏内科</text>
+							</view>
+							
+						</view>
+						<view class="mark">
+							有号
+						</view>
+					</view>
+					
 				</view>
 				
 			</view>
@@ -147,7 +197,7 @@
 		left: 0;
 		width: 100vw;
 		padding: 0 30upx 30upx;
-		background-color: #d244df;
+		background-color: #bcdfdf;
 		
 		.logo{
 			padding-top: 20upx;
@@ -162,7 +212,7 @@
 			align-items: center;
 			width: 100%;
 			height: 80upx;
-			font-size: 24upx;
+			font-size: 28upx;
 			padding-left: 40upx;
 			box-sizing: border-box;
 			border-radius: 28upx;
@@ -274,7 +324,7 @@
 					text-align: center;
 					height: 100%;
 					line-height: 80upx;
-					border-radius: 20upx;
+					border-radius: 0 0 20upx 20upx;
 					
 				}
 				.active{
@@ -350,9 +400,12 @@
 				height: 60upx;
 				line-height: 60upx;
 				border-radius: 30upx;
-				background-color: #00aaff;
-				color:#fff;
+				background-image:linear-gradient(to right, #f8f7ff 10%, #d2e0ff);
+				color:#747474;
 				font-size:28upx;
+				.light{
+					color:#319cff;
+				}
 			}
 			.doctor_bd{
 				margin-top: 20upx;
@@ -360,10 +413,69 @@
 				justify-content: space-between;
 				flex-wrap: wrap;
 				.doctorItem{
+					position: relative;
 					width: 48%;
 					height: 300upx;
-					border-radius: 10upx;
-					background-color: #d244df;
+					border-radius: 20upx;
+					margin-bottom:20upx;
+					.doctorImg{
+						width: 100%;
+						height: 100%;
+						border-radius: 20upx;
+						.doctor_avator{
+							width: 100%;
+							height: 100%;
+						}
+					}
+					.doctorInfo{
+						position: absolute;
+						width: 274upx;
+						left: 0;
+						bottom:0;
+						border-radius: 20upx 20upx 0 0;
+						padding: 20upx 30upx;
+						background-image:linear-gradient(to right, #b9e9ff, #e5f8ff);
+						.doctor_name{
+							display: flex;
+							align-items: center;
+							width: 370upx;
+							box-sizing: border-box;
+							
+							.names{
+								font-size:32upx;
+								margin-right: 10upx;
+								
+							}
+							.remark{
+								width: 60upx;
+								text-align: center;
+								height: 26upx;
+								line-height: 26upx;
+								background-color: #ffe895;
+								border-radius: 13upx;
+								font-size:26upx;
+								color:#ffb32e;
+							}
+						}
+						.skill{
+							font-size: 26upx;
+							color:#7c7c7c;
+						}
+					}
+					.mark{
+						position: absolute;
+						top: 0;
+						right: 0;
+						width: 80upx;
+						height: 36upx;
+						font-size:28upx;
+						border-radius: 18upx 18upx 0 18upx;
+						background-color: #3041de;
+						color:#fff;
+						text-align: center;
+						line-height:36upx;
+						
+					}
 				}
 			}
 			
