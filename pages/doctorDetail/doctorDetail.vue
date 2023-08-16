de3f8ba4287baed8209f348cb1018cf60719f412
wxd_code <w15755583882022@163.com>
2023-08-16 22:41:28 +0800

    底部按钮的搭建

--- pages/doctorDetail/doctorDetail.vue
+++ pages/doctorDetail/doctorDetail.vue
@@ -4,6 +4,11 @@
 			<image src="../../static/logo.png" mode=""></image>
 			<text>收藏按钮</text>
 		</view>
+		<view class="btn">
+			<view class="context">
+				预约服务
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -22,5 +27,26 @@
 	width: 100vw;
 	height: 100vh;
 	box-sizing: border-box;
+	.bannerImg{
+		
+	}
+	.btn{
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		padding: 20upx;
+		box-sizing: border-box;
+		.context{
+			background-color: #414dff;
+			color:#fff;
+			font-size: 32upx;
+			height: 80upx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			border-radius: 10upx;
+		}
+	}
 }
 </style>
